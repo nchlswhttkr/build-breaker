560c2770b19aee216c29fdf8a2507b9329fa5048
Nicholas Whittaker <nchlswhttkr@gmail.com>
2019-06-25 15:13:44 +1000

    Use Serverless transform
    This is much less verbose, thank you AWS!

--- src/tip/main.go
+++ src/tip/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func Handler(ctx context.Context, e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response, _ := json.Marshal(e.PathParameters)
 	return events.APIGatewayProxyResponse{
-		Body:       e.Body,
+		Body:       string(response[:]),
 		StatusCode: 201,
 	}, nil
 }
